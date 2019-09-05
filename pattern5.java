4d6cb705ab5f6c439972197853b8057cad0657fa
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2019-09-05 16:38:48 +0530

    Just another pattern.

---
+++ pattern5.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String args[])
+    {int k=1;
+        for(int i=1;i<=4;i++)
+        {
+        for(int j=0;j<k;j++)
+        {
+            System.out.print("#");
+        }
+        System.out.println();
+        k=k+2;
+        }
+    }
+}
